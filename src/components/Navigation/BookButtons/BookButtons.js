39e63bcc244b8745b5fe2da07faa1f693bc2ad71
Daniel Troyano <dtroyano@cox.net>
2019-11-03 09:14:03 -0600

    Worked on Header

---
+++ src/components/Navigation/BookButtons/BookButtons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import NavigationItem from '../NavigationItem/NavigationItem';
+
+import classes from './BookButtons.module.css';
+
+const bookButtons = (props) => (
+
+);
+
+export default bookButtons;
---
+++ src/components/Navigation/NavigationItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavigationItem from './NavigationItem/NavigationItem';
+
+import classes from './NavigationItems.module.css';
+
+const navigationItems = (props) => (
+    <ul className={classes.navigationItems}>
+        <NavigationItem link='/'>Home</NavigationItem>
+        <NavigationItem link='/'>Calendar</NavigationItem>
+        <NavigationItem link='/'>Contact</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
