f97a32fa7b56bd579f3f209e987e6e59c67b3111
Daniel Troyano <dtroyano@cox.net>
2019-11-03 17:50:00 -0600

    Drag and Drop Calendar

--- src/containers/Calendar/Calendar.js
+++ src/containers/Calendar/Calendar.js
@@ -1,180 +1,84 @@
 import React, { Component } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import moment from 'moment';
 
+import events from './events';
+
+const DragAndDropCalendar = withDragAndDrop(Calendar);
+
 class MyCalendar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { events: events };
+        this.moveEvent = this.moveEvent.bind(this);
+        this.newEvent = this.newEvent.bind(this);
+    }
+
+    moveEvent({ event, start, end, isAllDay: droppedOnAllDaySlot }) {
+        const { events } = this.state;
+        const idx = events.indexOf(event);
+        let allDay = event.allDay;
+
+        if (!event.allDay && droppedOnAllDaySlot) {
+            allDay = true;
+        } else if (event.allDay && !droppedOnAllDaySlot) {
+            allDay = false;
+        }
+
+        const updatedEvent = { ...event, start, end, allDay };
+
+        const nextEvents = [...events];
+        nextEvents.splice(idx, 1, updatedEvent);
+
+        this.setState({ events: nextEvents });
+    }
+
+    resizeEvent = ({ event, start, end }) => {
+        const { events } = this.state;
+
+        const nextEvents = events.map(existingEvent => {
+            return existingEvent.id === event.id
+                ? { ...existingEvent, start, end }
+                : existingEvent;
+        })
+
+        this.setState({ events: nextEvents });
+    }
+
+    newEvent(event) {
+        // let idList = this.state.events.map(a => a.id)
+        // let newId = Math.max(...idList) + 1
+        // let hour = {
+        //   id: newId,
+        //   title: 'New Event',
+        //   allDay: event.slots.length == 1,
+        //   start: event.start,
+        //   end: event.end,
+        // }
+        // this.setState({
+        //   events: this.state.events.concat([hour]),
+        // })
+    }
+
+
+
     render() {
         const localizer = momentLocalizer(moment);
 
-        const now = new Date();
-        const myEventsList = [
-            {
-                id: 0,
-                title: 'All Day Event very long title',
-                allDay: true,
-                start: new Date(2019, 3, 0),
-                end: new Date(2019, 3, 1),
-            },
-            {
-                id: 1,
-                title: 'Long Event',
-                start: new Date(2019, 3, 7),
-                end: new Date(2019, 3, 10),
-            },
-
-            {
-                id: 2,
-                title: 'DTS STARTS',
-                start: new Date(2016, 2, 13, 0, 0, 0),
-                end: new Date(2016, 2, 20, 0, 0, 0),
-            },
-
-            {
-                id: 3,
-                title: 'DTS ENDS',
-                start: new Date(2016, 10, 6, 0, 0, 0),
-                end: new Date(2016, 10, 13, 0, 0, 0),
-            },
-
-            {
-                id: 4,
-                title: 'Some Event',
-                start: new Date(2019, 3, 9, 0, 0, 0),
-                end: new Date(2019, 3, 10, 0, 0, 0),
-            },
-            {
-                id: 5,
-                title: 'Conference',
-                start: new Date(2019, 3, 11),
-                end: new Date(2019, 3, 13),
-                desc: 'Big conference for important people',
-            },
-            {
-                id: 6,
-                title: 'Meeting',
-                start: new Date(2019, 3, 12, 10, 30, 0, 0),
-                end: new Date(2019, 3, 12, 12, 30, 0, 0),
-                desc: 'Pre-meeting meeting, to prepare for the meeting',
-            },
-            {
-                id: 7,
-                title: 'Lunch',
-                start: new Date(2019, 3, 12, 12, 0, 0, 0),
-                end: new Date(2019, 3, 12, 13, 0, 0, 0),
-                desc: 'Power lunch',
-            },
-            {
-                id: 8,
-                title: 'Meeting',
-                start: new Date(2019, 3, 12, 14, 0, 0, 0),
-                end: new Date(2019, 3, 12, 15, 0, 0, 0),
-            },
-            {
-                id: 9,
-                title: 'Happy Hour',
-                start: new Date(2019, 3, 12, 17, 0, 0, 0),
-                end: new Date(2019, 3, 12, 17, 30, 0, 0),
-                desc: 'Most important meal of the day',
-            },
-            {
-                id: 10,
-                title: 'Dinner',
-                start: new Date(2019, 3, 12, 20, 0, 0, 0),
-                end: new Date(2019, 3, 12, 21, 0, 0, 0),
-            },
-            {
-                id: 11,
-                title: 'Birthday Party',
-                start: new Date(2019, 3, 13, 7, 0, 0),
-                end: new Date(2019, 3, 13, 10, 30, 0),
-            },
-            {
-                id: 12,
-                title: 'Late Night Event',
-                start: new Date(2019, 3, 17, 19, 30, 0),
-                end: new Date(2019, 3, 18, 2, 0, 0),
-            },
-            {
-                id: 12.5,
-                title: 'Late Same Night Event',
-                start: new Date(2019, 3, 17, 19, 30, 0),
-                end: new Date(2019, 3, 17, 23, 30, 0),
-            },
-            {
-                id: 13,
-                title: 'Multi-day Event',
-                start: new Date(2019, 3, 20, 19, 30, 0),
-                end: new Date(2019, 3, 22, 2, 0, 0),
-            },
-            {
-                id: 14,
-                title: 'Today',
-                start: new Date(new Date().setHours(new Date().getHours() - 3)),
-                end: new Date(new Date().setHours(new Date().getHours() + 3)),
-            },
-            {
-                id: 15,
-                title: 'Point in Time Event',
-                start: now,
-                end: now,
-            },
-            {
-                id: 16,
-                title: 'Video Record',
-                start: new Date(2019, 3, 14, 15, 30, 0),
-                end: new Date(2019, 3, 14, 19, 0, 0),
-            },
-            {
-                id: 17,
-                title: 'Dutch Song Producing',
-                start: new Date(2019, 3, 14, 16, 30, 0),
-                end: new Date(2019, 3, 14, 20, 0, 0),
-            },
-            {
-                id: 18,
-                title: 'Itaewon Halloween Meeting',
-                start: new Date(2019, 3, 14, 16, 30, 0),
-                end: new Date(2019, 3, 14, 17, 30, 0),
-            },
-            {
-                id: 19,
-                title: 'Online Coding Test',
-                start: new Date(2019, 3, 14, 17, 30, 0),
-                end: new Date(2019, 3, 14, 20, 30, 0),
-            },
-            {
-                id: 20,
-                title: 'An overlapped Event',
-                start: new Date(2019, 3, 14, 17, 0, 0),
-                end: new Date(2019, 3, 14, 18, 30, 0),
-            },
-            {
-                id: 21,
-                title: 'Phone Interview',
-                start: new Date(2019, 3, 14, 17, 0, 0),
-                end: new Date(2019, 3, 14, 18, 30, 0),
-            },
-            {
-                id: 22,
-                title: 'Cooking Class',
-                start: new Date(2019, 3, 14, 17, 30, 0),
-                end: new Date(2019, 3, 14, 19, 0, 0),
-            },
-            {
-                id: 23,
-                title: 'Go to the gym',
-                start: new Date(2019, 3, 14, 18, 30, 0),
-                end: new Date(2019, 3, 14, 20, 0, 0),
-            },
-        ];
         return (
             <div> THIS IS A CALENDAR
-            <Calendar
+            <DragAndDropCalendar
+                    selectable
                     localizer={localizer}
-                    events={myEventsList}
-                    startAccessor="start"
-                    endAccessor="end"
+                    events={this.state.events}
+                    onEventDrop={this.moveEvent}
+                    resizable
+                    onEventResize={this.resizeEvent}
+                    onSelectSlot={this.newEvent}
+                    onDragStart={console.log}
                     style={{ height: 1000 }} />
             </div >
 
